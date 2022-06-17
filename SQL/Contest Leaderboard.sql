222f627e5e2be956214fba91e8403a71ad17007f
Shivam Gupta <73192898+kumar455shivam@users.noreply.github.com>
2022-06-18 00:10:45 +0530

    Create Contest Leaderboard.sql

---
+++ SQL/Contest Leaderboard.sql
@@ -0,0 +1,16 @@
+-- https://www.hackerrank.com/challenges/contest-leaderboard/problem?h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+select h.hacker_id, name, sum(score) as total_score
+from
+hackers as h inner join
+/* find max_score*/
+(select hacker_id,  max(score) as score from submissions group by challenge_id, hacker_id) max_score
+
+on h.hacker_id=max_score.hacker_id
+group by h.hacker_id, name
+
+/* don't accept hackers with total_score=0 */
+having total_score > 0
+
+/* finally order as required */
+order by total_score desc, h.hacker_id
+;
